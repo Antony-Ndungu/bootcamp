1273a6f957711447ce1be7020369b0a79e227c71
Antony-Ndungu <anthony.ndungu@students.jkuat.ac.ke>
2016-03-22 14:52:57 +0300

    Added the function 'add_student'

---
+++ add_student.py
@@ -0,0 +1,17 @@
+#student dictionary
+student = {
+    'name': 'An Other',
+    'langs': ['Python', 'JavaScript', 'PHP'],
+    'age': 23
+}
+
+#An empty list of students
+    list_of_students = []
+
+
+def add_student(student):
+    """Adds a student dictionary to a list of students
+     @params student
+     @return doesn't return anything"""
+     list_of_students.append(student)
+    
